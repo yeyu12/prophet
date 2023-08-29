b8926c4d730c1e8b91e0dc6028ac24467fe05047
yeyu <lingchenyiduz@gmail.com>
2023-08-29 14:09:38 +0800

    这个工具还有点问题，不能使用，工具中的方法搜索，需要在全局变量MyDb中去搜索

--- astHook.js
+++ astHook.js
@@ -1,58 +1,92 @@
+const handleCode = require("./handleCode");
+const fs = require("fs");
 
-const handleCode =require("./handleCode")
+function astHook(requestDetail, responseDetail) {
+  let newResponse = Object.assign({}, responseDetail.response);
 
+  console.log("文件URL：----------", requestDetail.url)
+  // 跳过官方库
+  if (typeof requestDetail.url == "string" && requestDetail.url.match("jquery|layui|swiper|element|webpack|antd|ali-content-platform/content-publish")) {
+    return {
+      response: newResponse
+    };
+  }
 
-function astHook(requestDetail, responseDetail){
+  // 处理Js文件
+  if (typeof requestDetail.url == "string" && requestDetail.url.match(".js")) {
+    let hostname = requestDetail.requestOptions.hostname,
+      port = requestDetail.requestOptions.port,
+      path = requestDetail.requestOptions.path.replace(/[\<\>\:\"\/\\\|\?\*]+/g, "_"),
+      fileDir = "cache/" + hostname + "/" + port,
+      filePath = fileDir + "/" + path;
 
+    console.log("文件名：------------", path)
 
-    // 跳过官方库
+    //从缓存目录中读取
+    try {
+      if (fs.existsSync(filePath)) {
+        newResponse.body = fs.readFileSync(filePath, {
+          flag: "r",
+          encoding: "utf8",
+        }).toString();
 
-    if( typeof requestDetail.url == "string" && (requestDetail.url.match("jquery") || requestDetail.url.match("layui")|| requestDetail.url.match("swiper") || requestDetail.url.match("element"))){
         return {
-            response: responseDetail.response
+          response: newResponse
+        };
+      }
+    } catch (err) {
+      console.log("缓存文件不存在！")
+    }
+
+    let body = newResponse.body.toString();
+    try {
+      //处理js内容
+      let newBody = handleCode.handleJsCode(body);
+      newResponse.body = newBody;
+
+      //创建缓存目录
+      let fileDirNode = ".";
+      fileDir.split("/").forEach(function (k) {
+        fileDirNode += "/" + k;
+        if (fs.existsSync(fileDirNode)) {
+          return;
         }
+
+        fs.mkdirSync(fileDirNode, 0o777);
+      });
+
+      //写入缓存文件
+      fs.writeFileSync(filePath + ".source.js", body, {
+        encoding: "utf8",
+        flag: "w+"
+      });
+      fs.writeFileSync(filePath, newBody, {
+        encoding: "utf8",
+        flag: "w+"
+      });
+    } catch (err) {
+      console.log("解析js文件错误:", err, requestDetail.url);
+      return null;
     }
-
-    // 处理Js文件
-
-    if( typeof requestDetail.url == "string" && requestDetail.url.match(".js")  && responseDetail.response.header['Content-Type'] && responseDetail.response.header['Content-Type'].match("javascript")){
-        let newResponse= responseDetail.response;
-
-
-        let body = newResponse.body.toString();
-
-        try{
-            let newBody = handleCode.handleJsCode(body)
-            newResponse.body = newBody
-        }catch(err){
-            console.log("解析js文件错误:",err,requestDetail.url)
-
-            return null
-        }
-
-
-        return {response:newResponse}
-    }
-
-
-    // 处理Html文件
-    if (typeof requestDetail.url == "string" && responseDetail.response.header['Content-Type'] && responseDetail.response.header['Content-Type'].match("text/html")){
-        let newResponse= responseDetail.response;
-
-
-
-        let body = newResponse.body.toString();
-
-        let newBody = handleCode.handleHtmlCode(body)
-        newResponse.body = newBody
-        return {response:newResponse}
-    }
-
 
     return {
-            response: responseDetail.response
-        }
+      response: newResponse
+    };
+  }
 
-    }
+  // 处理Html文件
+  if (typeof requestDetail.url == "string" && newResponse.header["Content-Type"] && newResponse.header["Content-Type"].match("text/html")) {
+    let body = newResponse.body.toString();
+    let newBody = handleCode.handleHtmlCode(body);
+    newResponse.body = newBody;
+    return {
+      response: newResponse
+    };
+  }
+
+  return {
+    response: responseDetail.response,
+  };
+}
 
 module.exports.astHook = astHook
